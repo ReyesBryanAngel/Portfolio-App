43b1f7bee36002b0b60f516437e50992a3474e39
CoderBryan05 <bryan@spotter.ai>
2025-07-12 11:46:29 +0800

    apply: details changes

--- portfolio-app/src/components-v2/ExperienceSection.jsx
+++ portfolio-app/src/components-v2/ExperienceSection.jsx
@@ -1,43 +1,50 @@
 export default function ExperienceSection() {
   const experiences = [
     {
-      role: "Frontend Developer",
-      company: "Spotter.ai",
-      duration: "December 2024 – June 2025",
-      workType: "Project- based",
-      description:
-        "Participated in the frontend development of Spotter.ai’s trucking management system. Collaborated with cross-functional teams to deliver responsive dashboards, fix complex Front end bugs, and develop table templates features with backend integration.",
-      technologies: [
-        "React",
-        "Tailwind CSS",
-        "Material UI",
-        "RESTful API",
-        "GIT",
+      role: "Full Stack Software Engineer",
+      company: "Yondu Inc.",
+      duration: "April 2024 - Present",
+      workType: "Full-time",
+      description: [
+        "Developing and integrating secure, enterprise-grade applications for insurance clients under Yondu Inc.",
+        "Integrated AWS Cognito (User Pool) for secure user authentication into the MS BPI Insurance system, aligning with enterprise security standards ",
+        "Assigned as the Frontend Developer for the Renewal Quotation Tool used by Pacific Cross, improving their internal policy management and renewal workflows Collaborated with stakeholders and backend engineer to ensure seamless integration of authentication, business logic, and UI components ",
       ],
+      // description:
+      //   "Participated in the frontend development of Spotter.ai’s trucking management system. Collaborated with cross-functional teams to deliver responsive dashboards, fix complex Front end bugs, and develop table templates features with backend integration.",
+      technologies: ["React", "Nest JS", "AWS", "GIT"],
     },
     {
       role: "Backend Developer",
       company: "Gametime Solutions Inc.",
-      duration: "March 2024 – November 2024",
+      duration: "March 2023 – April 2024",
       workType: "Full Time",
-      description:
-        "Developed the backend of betting app gaming platform (Easy2/Easy3). Integrated winner declaration, balance systems, and built an API web socket for real time data appearances of winning amounts and game results declaration per round.",
-      technologies: ["HTML", "CSS", "JavaScript", "Git", "PHP"],
+      description: [
+        "Developed and maintained backend services for real-time gaming and admin dashboard systems.",
+        "Designed and built backend APIs for Easy 2 and Easy 3 gaming platforms, handling player entry, draw results, and transactions",
+        "Integrated real-time winner declaration and balance updates using WebSocket technology.",
+        "Maintained and enhanced internal admin dashboard supporting operator workflows and reporting.",
+      ],
+      technologies: ["PHP", "MySQL", "WebSocket", "REST API", "AWS"],
     },
     {
       role: "Full Stack Developer",
       company: "FinXP",
-      duration: "July 2022 – March 2024",
+      duration: "July 2021 – March 2023",
       workType: "Full Time",
-      description:
-        "Developed major features for the FinXP Admin Portal including the Maltese Creditor Identifier (MCI) and built the Apply Corporate Application. Focused on scalable UI, complex API integrations, and well maintained codes for better reusability and smooth feature enhancement.",
+      description: [
+        "Built and maintained core business applications for a fintech company serving EU-based clients.",
+        "Implemented the Maltese creditor identifier feature in the client portal, improving regulatory compliance.",
+        "Integrated APIs and developed backend model structures to support financial application-requirement workflows.",
+        "Developed the Apply Corporate application for client onboarding and document verification.",
+      ],
       technologies: [
         "React",
-        "Node.js",
-        "Express",
-        "MongoDB",
         "Laravel",
-        "PHP",
+        "Nest JS",
+        "MySQL",
+        "Azure",
+        "KeyCloak",
       ],
     },
   ];
@@ -59,7 +66,12 @@ export default function ExperienceSection() {
               </div>
             </div>
             <p className="text-gray-400 text-sm">{exp?.workType}</p>
-            <p className="text-gray-300 mt-4">{exp.description}</p>
+            {/* <p className="text-gray-300 mt-4">{exp.description}</p> */}
+            <ul className="text-sm mt-2 list-disc ml-5">
+              {exp?.description?.map((desc, idx) => (
+                <li key={idx}>{desc}</li>
+              ))}
+            </ul>
             <div className="mt-4 flex flex-wrap gap-2">
               {exp.technologies.map((tech, i) => (
                 <span
